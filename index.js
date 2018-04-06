67ca01e070d3d2a058bcb1d6df5e3c1d2ac50876
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-06 00:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, array.length - 1)
+  return kittens.slice(0, array.length - 1)
 }
 
 function removeFirstKitten() {
